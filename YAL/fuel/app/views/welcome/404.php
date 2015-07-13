1427df2ed7b7d0d6daf45ea98bf4e53bbe454d36
kentasuzuki <yal.yourartlife@gmail.com>
2015-07-14 01:02:56 +0900

    /app/fuel/views/welcome404.php
    41行目に "aaa" を追加。

--- YAL/fuel/app/views/welcome/404.php
+++ YAL/fuel/app/views/welcome/404.php
@@ -38,7 +38,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h1><?php echo $title; ?> <small>We can't find that!</small></h1>
+				<h1>aaa<?php echo $title; ?> <small>We can't find that!</small></h1>
 				<hr>
 				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
 				<p>This view is located at <code>APPPATH/views/welcome/404.php</code>.</p>
